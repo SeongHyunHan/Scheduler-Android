1b9edfc3e299eb959d6e17b3f639070cfd4201c1
Seong Hyun Han <seong.han@mohawkcollege.ca>
2018-03-14 17:00:03 -0400

    Get Notification and Add FAB

--- app/src/main/java/com/seong/scheduler/MyFirebaseMessagingService.java
+++ app/src/main/java/com/seong/scheduler/MyFirebaseMessagingService.java
@@ -1,9 +1,15 @@
 package com.seong.scheduler;
 
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONObject;
+
+import java.util.Map;
 
 /**
  * Created by hans on 2/26/2018.
@@ -15,6 +21,20 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.d(TAG, "onMessageReceived function called");
+
+        Log.d(TAG, "From" + remoteMessage.getFrom());
+
+        if(remoteMessage.getData().size() > 0){
+            Log.d(TAG, "Message data payload " + remoteMessage.getData());
+            Map<String, String> data = remoteMessage.getData();
+            Log.d(TAG,data.get("body"));
+
+        }
+
+        if(remoteMessage.getNotification() != null){
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification());
+        }
+
         super.onMessageReceived(remoteMessage);
     }
 
